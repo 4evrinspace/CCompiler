16275a513880123fcfb2ecb2531405a35a3747d4
Denis <dvavramenko@ispras.ru>
2025-04-08 21:10:21 +0300

    Main and small test

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "compiler.h"
+#include "riscv.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* input_file = fopen(argv[1], "r");
+    if (!input_file) {
+        fprintf(stderr, "Error: Cannot open file %s\n", argv[1]);
+        return 1;
+    }
+
+    yyin = input_file;
+    
+    printf("Starting compilation of %s...\n", argv[1]);
+    
+    if (yyparse() == 0) {
+        printf("Parsing successful!\n");
+        
+        
+        char* output_filename = "output.s";
+        FILE* output_file = fopen(output_filename, "w");
+        if (!output_file) {
+            fprintf(stderr, "Error: Cannot create output file %s\n", output_filename);
+            fclose(input_file);
+            return 1;
+        }
+        
+        generate_riscv_code(root, output_file);
+        fclose(output_file);
+        printf("RISC-V assembly generated in %s\n", output_filename);
+    } else {
+        fprintf(stderr, "Compilation failed at line %d\n", yylineno);
+    }
+
+    free_ast(root);
+    fclose(input_file);
+    return 0;
+} 
---
+++ test.c
@@ -0,0 +1,17 @@
+int main() {
+    int x;
+    int y;
+    
+    x = 10;
+    y = 20;
+    
+    if (x < y) {
+        x = x + 1;
+    }
+    
+    while (x < 100) {
+        x = x * 2;
+    }
+    
+    return 0;
+} 
